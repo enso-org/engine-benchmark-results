a2a0e24da197cb5cec5b8c0a213cf5c4ddff77c5
Pavel Marek <pavel.marek@enso.org>
2024-06-20 14:35:36 +0200

    Display only MAX_LABELS on MainPage

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,7 @@ declare const FS_URL: string
  * How many days of data to fetch by default
  */
 declare const DEFAULT_DAYS_TO_FETCH: number
+/**
+ * Maximum number of labels to display - how many benchmark graphs will be shown
+ */
+declare const MAX_LABELS: number
