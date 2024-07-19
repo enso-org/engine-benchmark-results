8226a5380ca54750de1a6243174bd566077f8e88
Pavel Marek <pavel.marek@enso.org>
2024-07-19 19:05:42 +0200

    datapointStore stores points by label

--- src/stores/dataPointsStore.ts
+++ src/stores/dataPointsStore.ts
@@ -4,12 +4,18 @@ import type { BenchDataPoint } from '../utils/data'
 export const useDataPointStore = defineStore('dataPoint', {
   state: () => ({
     dataPoints: [] as BenchDataPoint[],
+    byLabels: new Map() as Map<string, BenchDataPoint[]>,
   }),
 
   actions: {
     addDataPoint(dataPoint: BenchDataPoint) {
       console.assert(!this.containsDatapoint(dataPoint))
       this.dataPoints.push(dataPoint)
+      if (this.byLabels.has(dataPoint.label)) {
+        this.byLabels.get(dataPoint.label)!.push(dataPoint)
+      } else {
+        this.byLabels.set(dataPoint.label, [dataPoint])
+      }
     },
 
     findDataPointsOnBenchRun(benchRunId: string): BenchDataPoint[] {
@@ -22,6 +28,22 @@ export const useDataPointStore = defineStore('dataPoint', {
       label?: string
       branch?: string
     }): BenchDataPoint[] {
+      if (options.label) {
+        console.assert(this.byLabels.has(options.label))
+        const dpForLabel = this.byLabels.get(options.label)!
+        return dpForLabel.filter((dp) => {
+          if (options.startDate && dp.benchRun.headCommit.timestamp < options.startDate) {
+            return false
+          }
+          if (options.endDate && dp.benchRun.headCommit.timestamp > options.endDate) {
+            return false
+          }
+          if (options.branch && dp.benchRun.branch !== options.branch) {
+            return false
+          }
+          return true
+        })
+      }
       return this.dataPoints.filter((dp) => {
         if (options.startDate && dp.benchRun.headCommit.timestamp < options.startDate) {
           return false
