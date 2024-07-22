27d883658d69305e4c3b24b3d54a318c5dd4bbce
Pavel Marek <pavel.marek@enso.org>
2024-07-22 19:15:43 +0200

    startDate and endDate is provided as route

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <Suspense>
-    <MainPage />
+    <RouterView />
   </Suspense>
 </template>
 
 <script setup lang="ts">
-import MainPage from './components/MainPage.vue'
+import { subDays } from 'date-fns'
+import { useRouter } from 'vue-router'
+
+const startDate = subDays(MAX_DATE, DEFAULT_DAYS_TO_FETCH)
+const endDate = new Date(MAX_DATE)
+
+const router = useRouter()
+router.push({
+  path: '/',
+  query: {
+    startDate: startDate.toISOString(),
+    endDate: endDate.toISOString(),
+  },
+})
 </script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import App from './App.vue'
 
+import { createMemoryHistory, createRouter } from 'vue-router'
+import MainPage from './components/MainPage.vue'
+
+const routes = [{ path: '/', component: MainPage }]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
 const app = createApp(App)
 const pinia = createPinia()
 const vuetify = createVuetify({
@@ -13,4 +23,5 @@ const vuetify = createVuetify({
 })
 app.use(pinia)
 app.use(vuetify)
+app.use(router)
 app.mount('#app')
