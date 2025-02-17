44d7ce84d2ce7a07045b5c9cbd02da1183381862
Pavel Marek <pavel.marek@enso.org>
2025-02-17 13:49:53 +0100

    Update vite-config.
    - Copy cache dir inside dist
    - Update URLs to point to the served repo
    - MIN_DATE is a global constant

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,39 @@
 import vue from '@vitejs/plugin-vue'
 import serveStatic from 'serve-static'
 import { ViteDevServer, defineConfig, type Plugin } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import vuetify from 'vite-plugin-vuetify'
 
-const PORT = 5179
-const FS_URL = process.env.NODE_ENV === 'development' ? `http://localhost:${PORT}` : '/'
+const LOCAL_FS_PORT = 5179
+const REPO_NAME = '/engine-benchmark-results/'
+const REPO_URL = 'https://enso-org.github.io/engine-benchmark-results'
+const FS_URL = isDevBuild() ? `http://localhost:${LOCAL_FS_PORT}` : REPO_URL
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true }), fsServer()],
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }),
+    fsServer(),
+    // Copy all the cache files to `dist/cache`.
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'cache/*',
+          dest: 'cache',
+        },
+      ],
+    }),
+  ],
   define: {
     FS_URL: JSON.stringify(FS_URL),
     DEFAULT_DAYS_TO_FETCH: 30,
     MAX_LABELS: 10,
+    MIN_DATE: JSON.stringify('2022-12-13'),
   },
+  base: isDevBuild() ? '/' : REPO_NAME,
   server: {
-    port: PORT,
+    port: LOCAL_FS_PORT,
     strictPort: true,
   },
 })
@@ -27,4 +45,8 @@ function fsServer(): Plugin {
       server.middlewares.use('/cache', serveStatic('/cache'))
     },
   }
+}
+
+function isDevBuild(): boolean {
+  return process.env.NODE_ENV === 'development'
 }
